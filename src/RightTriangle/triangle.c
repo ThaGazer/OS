e3aa1018d83ce74424f49ee0289ae9c5636aeb6c
Justin Ritter <Justin_Ritter@baylor.edu>
2018-09-03 04:34:52 -0500

    c programming side v1

--- src/RightTriangle/triangle.c
+++ src/RightTriangle/triangle.c
@@ -8,104 +8,114 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <semaphore.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
-#include <math.h>
+
+struct point {
+  int x, y;
+} typedef Point
+
+struct triangle {
+  Point p1, p2, p3;
+} typedef Triangle
+
+bool isRight(Triangle t);
+bool contains(Triangle t, Triangle* list);
 
 int main(int argc, char** argv) {
 
   //check parameter requirements
   if(argc != 3) {
-    fprintf(stderr, "incorrect number of parameters:%d\n", argc-1); 
+    fprintf(stderr, "Usage: <fileName> <nprocs>", argc-1);
     return -1;
   }
 
-  int centX = 0, centY = 0, rad = 1;
   char* fname = argv[1];
-  int nProc = atoi(argv[2]);
-  int fd[nProc][2];
-  int amount[nProc];
-  int startPos[nProc];
-  int totPnts;
-  char** line;
+  int nprocs = atoi(argv[2]);
+  int fd[nprocs][2];
+  int totalPoints;
 
-  //parents pid
+  //pids
   pid_t Ppid = getpid();
   pid_t Cpid;
 
   //file checker
   FILE* fileId;
   if(!(fileId = fopen(fname, "r"))) {
-    perror("could not open file");
+    perror("could not open file\n");
     return -1;
   }
 
   //calculates how much each child should handle
-  fscanf(fileId, "%d", &totPnts);
+  fscanf(fileId, "%d", &totalPoints);
   fgetc(fileId);
-  int amnt = totPnts/nProc;
 
-  int sum = 0, strtPos = 0;
-  for(int i = 0; i < nProc; i++) {
-    amount[i] = amnt;
-    startPos[i] = strtPos;
-    strtPos += amnt;
-    sum += amnt;
-  }
-  //adds extra points to last process
-  if(sum < totPnts) {
-    amount[nProc-1] += (totPnts - sum);
-  }
+  //reads and stores all the points in the file
+  Point* pointList = malloc(totalPoints * sizeof(Point));
+  int xCoord, yCoord, space;
+  for(int i = 0; i < totalPoints; i++) {
+    xCoord = fgetc(fileId);
+    space = fgetc(fileId);
+    yCoord = fgetc(fileId);
 
-  //reads and stores the file
-  line = malloc(totPnts * sizeof(char*));
-  int c;
-  for(int i = 0; i < totPnts; i++) {
-    line[i] = malloc(80 * sizeof(char));
-
-    int j;
-    for(j = 0;(c = fgetc(fileId)) != '\n'; j++) {
-      if(c == EOF) {
-        perror("wrong number of points");
-        exit(-1);
-      }
-      line[i][j] = c;
+    if(xCoord != '\n' && yCoord != '\n' && space != '\n') {
+      pointList[i].x = atoi(xCoord);
+      pointList[i].y = atoi(yCoord);
+    } else {
+      perror("incorrect file formatting\n");
     }
-    line[i][j] = '\n';
   }
 
   //close file pointer
   fclose(fileId);
 
+  int totalLoad = ((totalPoints * totalPoints * totalPoints) / 6) - (totalPoints / 6);
+  int workLoad = totalLoad/nProc;
+  Triangle foundTriangles[] = new malloc((totalPoints*2) * sizeof(Triangle));
+
   //creates child processes
   for(int i = 0; i < nProc; i++) {
+    //in parent process
     if(Ppid == getpid()) {
+
+      //creates pipe
       pipe(fd[i]);
-      write(fd[i][1], &amount[i], sizeof(int));
-      write(fd[i][1], &startPos[i], sizeof(int));
+
+      //what to send to child
 
       if((Cpid = fork()) == -1) {
         fprintf(stderr, "fork failed: %d\n", getpid());
         return -1;
       }
     }
+    //in child process
     if(Cpid == 0) { 
-      int howmuch, where;
-      read(fd[i][0], &howmuch, sizeof(int));
-      read(fd[i][0], &where, sizeof(int));
 
-      int count;
-      for(int i = where; i < (howmuch+where); i++) {
-        double x = atof(strtok(line[i], ", "));
-        double y = atof(strtok(NULL, "\n"));
-
-        if((sqrt(x - centX) + sqrt(y - centY)) <= sqrt(rad)) {
-          count++; 
+      int i,j,k, amount;
+      bool start = true;
+      for(; i < totalPoints && amount > 0; i++) {
+        if(!start) {
+          j = i+1;
         }
-      } 
-      return 0;
+        for(; j < totalPoints && amount > 0; j++) {
+          if(!start) {
+            k = j;
+          }
+          for(; k < totalPoints && amount > 0; k++) {
+            if(start) {
+              start = false;
+            }
+
+            Point p1 = pointList[i], p2 = pointList[j], p3 = pointList[k];
+            if(contains(p1,p2,p3, foundTriangles) && isRight(p1,p2,p3)) {
+            }
+
+          }
+        }
+      }
     }    
   }
 
@@ -122,9 +132,6 @@ int main(int argc, char** argv) {
     read(fd[i][1], &pntIn, sizeof(int));
     totPntIn += pntIn;
   }
-
-  double pi = 4 * (totPntIn/totPnts);
-  printf("pi is equal to: %f\n", pi);
 
   //free file mem
   if(line) {
