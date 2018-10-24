9489aa6a8addb812407c0a19990bf1411f841dc0
Justin Ritter <Justin_Ritter@baylor.edu>
2018-10-24 02:59:07 -0500

    created fileio

---
+++ src/fileio/DataSource.java
@@ -0,0 +1,38 @@
+/*
+ * Author: Justin Ritter
+ * File: null.java
+ * Date: 10/20/2018
+ */
+package fileio;
+
+import java.io.IOException;
+
+/**
+ * A source of bytes with transactions that can read/write bytes
+ *
+ * version 1.0
+ */
+public interface DataSource extends AutoCloseable {
+
+    /**
+     * Get a new transaction for reading/writing source bytes
+     *
+     * @return new transaction
+     */
+    Transaction newTransaction();
+
+    /**
+     * Get the number of bytes in the data source currently
+     *
+     * @return number of bytes in source
+     * @throws IOException if error determining the number of bytes
+     */
+    long getLength() throws IOException;
+
+    /**
+     * Close all open transactions and then this data source
+     *
+     * @throws IOException if an I/O occurs
+     */
+    void close() throws IOException;
+}
---
+++ src/fileio/DeadlockDetectedException.java
@@ -0,0 +1,47 @@
+/*
+ * Author: Justin Ritter
+ * File: null.java
+ * Date: 10/20/2018
+ */
+package fileio;
+
+import java.io.IOException;
+
+/**
+ * Thrown to indicate an operation (if allowed) may have caused deadlock
+ */
+public class DeadlockDetectedException extends IOException {
+
+    private static final long serialVersionUID = 1L;
+
+    private String cycle;
+
+    /**
+     * Constructs exception with given cycle and null message
+     *
+     * @param cycle string representation of cycle causing deadlock
+     */
+    public DeadlockDetectedException(final String cycle) {
+        this(null, cycle);
+    }
+
+    /**
+     * Constructs exception with given message and cycle and n
+     *
+     * @param message detail message
+     * @param cycle string representation of cycle causing deadlock
+     */
+    public DeadlockDetectedException(final String message, final String cycle) {
+        super(message);
+        this.cycle = cycle;
+    }
+
+    /**
+     * Get a string representation of cycle
+     *
+     * @return string representative of cycle
+     */
+    public String getCycle() {
+        return cycle;
+    }
+}
